009205b9ffab9f616507f0900c6a4243bdb5f03e
ElMehdiTafnil <elmehdi.tafnil@gmail.com>
2024-10-13 08:17:11 +0100

    worked on the home page design

--- config/application.rb
+++ config/application.rb
@@ -11,10 +11,8 @@ module VibeMatch
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    # Add the assets/videos directory to the asset pipeline
+    config.assets.paths << Rails.root.join('app', 'assets', 'videos')
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -25,3 +23,4 @@ module VibeMatch
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
+
